3b6cf3a059fe819f4f56035a8970e1ba4d8377ce
Fabrizio Zeballos <54884274+FabrizioZeb@users.noreply.github.com>
2021-05-22 20:02:43 +0200

    Delete PhysicsSimulator directory

--- PhysicsSimulator/src/simulator/control/StateComparator.java
+++
@@ -1,7 +0,0 @@
-package simulator.control;
-
-import org.json.JSONObject;
-
-public interface StateComparator {
-	boolean equal(JSONObject s1, JSONObject s2);
-}
--- PhysicsSimulator/src/simulator/model/ForceLaws.java
+++
@@ -1,7 +0,0 @@
-package simulator.model;
-
-import java.util.List;
-
-public interface ForceLaws {
-	public void apply(List<Body> bs);
-}
